e6f8485763844f6dc1db48be959203a19dedb95b
Rootul Patel <rootulp@palantir.com>
2017-03-11 19:58:03 +0000

    Add xo and updateNotifier

--- cli.js
+++ cli.js
@@ -2,14 +2,17 @@
 'use strict';
 var request = require('request');
 var meow = require('meow');
+var updateNotifier = require('update-notifier');
 
-const DRIZZY_API = 'http://drizzy-server.herokuapp.com'
+const DRIZZY_API = 'http://drizzy-server.herokuapp.com';
 
 var cli = meow([
   'Usage',
   '  $ drizzy',
   ''
 ]);
+
+updateNotifier({pkg: cli.pkg}).notify();
 
 request(DRIZZY_API + '/lyric', function (error, response, body) {
   console.log(body);
