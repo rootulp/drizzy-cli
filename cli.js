c5f2e716576e3a5bf64a4578220b0f5d75d6ef47
Rootul Patel <rootulp@palantir.com>
2017-03-11 19:43:38 +0000

    Add meow

--- cli.js
+++ cli.js
@@ -1,8 +1,15 @@
 #! /usr/bin/env node
 'use strict';
 var request = require('request');
+var meow = require('meow');
 
 const DRIZZY_API = 'http://drizzy-server.herokuapp.com'
+
+var cli = meow([
+  'Usage',
+  '  $ drizzy',
+  ''
+]);
 
 request(DRIZZY_API + '/lyric', function (error, response, body) {
   console.log(body);
