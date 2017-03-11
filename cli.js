46cf9395fc44b21049553d6ad8fc51f49e1ce6d9
Rootul Patel <rootulp@palantir.com>
2017-03-11 20:07:23 +0000

    Use ora spinner

--- cli.js
+++ cli.js
@@ -1,8 +1,10 @@
 #! /usr/bin/env node
 'use strict';
-var request = require('request');
-var meow = require('meow');
-var updateNotifier = require('update-notifier');
+const request = require('request');
+const meow = require('meow');
+const ora = require('ora');
+const updateNotifier = require('update-notifier');
+
 
 const DRIZZY_API = 'http://drizzy-server.herokuapp.com';
 
@@ -14,6 +16,10 @@ var cli = meow([
 
 updateNotifier({pkg: cli.pkg}).notify();
 
+const spinner = ora().start();
 request(DRIZZY_API + '/lyric', function (error, response, body) {
-  console.log(body);
+  spinner.stopAndPersist({
+    "symbol": "🎧 ",
+    "text": body
+  });
 });
