469d35288b3ca1cfa4e9f0bf181e6670f7b9b9cc
Rootul Patel <rootulp@palantir.com>
2017-03-11 21:59:41 +0000

    Switch to request-promise
    Also use standard code style > xo

--- cli.js
+++ cli.js
@@ -1,22 +1,33 @@
 #! /usr/bin/env node
-'use strict';
-const request = require('request');
-const meow = require('meow');
-const chalk = require('chalk');
-const ora = require('ora');
+'use strict'
+const request = require('request')
+const rp = require('request-promise')
+const meow = require('meow')
+const chalk = require('chalk')
+const ora = require('ora')
+const updateNotifier = require('update-notifier')
+const pkg = require('./package.json')
 
-const DRIZZY_API = 'http://drizzy-server.herokuapp.com';
+const DRIZZY_API = 'http://drizzy-server.herokuapp.com'
 
-const cli = meow([
+meow([
   'Usage',
   '  $ drizzy',
   ''
-]);
+])
 
-const spinner = ora().start();
-request(DRIZZY_API + '/lyric', function (error, response, body) {
-  spinner.stopAndPersist({
-    symbol: '🎧 ',
-    text: chalk.blue(body)
-  });
-});
+updateNotifier({pkg}).notify()
+
+const spinner = ora().start()
+const options = {
+  method: 'GET',
+  uri: DRIZZY_API + '/lyric'
+}
+
+rp(options)
+  .then((data) => {
+    spinner.stopAndPersist({
+      symbol: '🎧 ',
+      text: chalk.blue(data)
+    })
+  })
