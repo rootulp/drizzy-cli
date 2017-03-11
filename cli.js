d04aa25a2fd47ea754726ce7ce326083b699b5c4
Rootul Patel <rootulp@palantir.com>
2017-03-11 19:29:55 +0000

    Remove error and statusCode output

--- cli.js
+++ cli.js
@@ -1,9 +1,9 @@
 #! /usr/bin/env node
-
+'use strict';
 var request = require('request');
 
-request('http://drizzy-server.herokuapp.com/lyric', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+const DRIZZY_API = 'http://drizzy-server.herokuapp.com'
+
+request(DRIZZY_API + '/lyric', function (error, response, body) {
+  console.log(body);
 });
